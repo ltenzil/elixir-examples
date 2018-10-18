d64451a37a72160c95e1cf83338d5ec5a9d16ddf
Tenzil <lourdino.tenzil@imaginea.com>
2018-10-18 21:22:45 +0530

    List with length and square calculations

---
+++ my_list.exs
@@ -0,0 +1,47 @@
+defmodule MyList do
+
+  # Count
+
+    def len([]), do: 0
+
+    """
+    len([]) runs when it matches arguments with empty list
+    """
+
+    def len([_ | tail]), do: 1 + len(tail)
+
+    """
+    len([_ | tail ]) runs when it matches arguments with list
+    ex: [1,2], [1],
+    explanation: list is always combination of head and tail, where head is the first value and tail is collection of rest of the values in list format.
+    In [1, 2, 3] => head : 1 and tail => [2, 3].
+    And this runs only when there is head and tail
+    [1, 2] => 1 + (len(2) => 1 + (len([]) => 0)) thus the sum is 1 + 1 + 0 = 2
+    """
+
+  # Square
+
+    def sq([]), do: []
+
+    """
+    similarly sq([]) runs when it matches arguments with empty list
+    """
+
+    def sq(n) when is_number(n), do: n * n
+
+    """
+    sq(n) runs when it matches arguments with anything, to overcome this we have guard clause is_number(n) this makes the sq() to be called only on numbers
+    """
+
+    def sq([head | tail]), do: [head * head | sq(tail)]
+
+end
+
+IO.puts(MyList.len([1,3,54]) |> MyList.sq)
+
+IO.puts(MyList.sq([1,3,54]) |> MyList.len)
+
+IO.puts(MyList.len([]))
+IO.puts(MyList.len([1,2,3,0]))
+IO.puts(MyList.sq([2,4,5,0]))
+
