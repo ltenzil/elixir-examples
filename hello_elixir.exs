d24ee3c3e806b8a1ec26e156645834489aa1dbc4
Tenzil <lourdino.tenzil@imaginea.com>
2020-01-06 16:30:23 +0530

    Docs added to hello elixir

--- hello_elixir.exs
+++ hello_elixir.exs
@@ -1,10 +1,31 @@
 defmodule HelloElixir do
 
+  @moduledoc """
+    This is the Hello module, here we are going to print hello world, and other values passed,
+    We do pattern matching and print the respective values.
+  """
+  @doc """
+    Prints Hello Ten when ten is passed as a argument, Argument is pattern matched here
+  """
   def print("ten"), do: "Hello Ten"
+
+  @doc """
+    Prints Hello World when :world is passed as a argument, Argument is pattern matched here
+  """
+
   def print(:world), do: "Hello World"
+
+  @doc """
+    Prints Hello Pramati when pramati is passed as a argument, Argument is pattern matched here
+  """
+
   def print("pramati"), do: "Hello Pramati"
 
   # def print(nil), do: "Printing Nil"
+
+  @doc """
+    this picks any argument, Do case and a pattern matching and prints accordinly
+  """
 
   def print(arg) do
     case arg do
@@ -14,6 +35,10 @@ defmodule HelloElixir do
       _ -> arg
     end
   end
+
+  @doc """
+    This is pattern matched but never called as the above method picks it. if we move this above then it works
+  """
 
   def print(nil), do: "Printing Nil"
 
